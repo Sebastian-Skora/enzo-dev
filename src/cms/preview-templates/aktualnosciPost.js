994a831339f4a39f921e7035a1850a3be24509b7
Sebskora <sebastian.skora77@gmail.com>
2021-06-22 14:29:27 +0200

    fix

--- src/cms/preview-templates/aktualnosciPost.js
+++ src/cms/preview-templates/aktualnosciPost.js
@@ -1,15 +1,15 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import BlogPostTemplate from '../../templates/post';
+import { BlogPostTemplate } from '../../templates/post';
 
 const AktualnosciPostPreview = ({ entry, widgetFor }) => {
-    const tags = entry.getIn(['data', 'tags'])
+
     return (
         <BlogPostTemplate
             content={widgetFor('body')}
             description={entry.getIn(['data', 'description'])}
-            tags={tags && tags.toJS()}
+
             title={entry.getIn(['data', 'title'])}
         />
     )
