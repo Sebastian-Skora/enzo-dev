0677bbc9a887725723c6c7672c74bb3249b92ac6
Sebskora <sebastian.skora27@gmail.com>
2022-04-05 21:20:15 +0200

    asdadsa

--- src/components/layout/MobileNav/MobileNavElements/MobileNavElements.js
+++ src/components/layout/MobileNav/MobileNavElements/MobileNavElements.js
@@ -3,12 +3,11 @@ import * as styles from "./MobileNavElements.module.scss";
 import { Link as NavLink } from "gatsby";
 import * as actions from "../../../redux/actions/index";
 import { connect } from "react-redux";
-import { gsap } from "gsap";
 
 const navigation_elements = [
   {
     path: "/",
-    name: "Strona główna",
+    name: "Home",
   },
   {
     path: "/uslugi/",
@@ -16,7 +15,7 @@ const navigation_elements = [
   },
   {
     path: "/slownik/",
-    name: "Słownik IT",
+    name: "Słownik",
   },
   {
     path: "/blog/",
@@ -31,52 +30,53 @@ const navigation_elements = [
 const MobileNavElements = ({ isOpen, toggleNav }) => {
   useEffect(() => {
     const ham = document.querySelector("#burger-button");
-    const menu = document.querySelector("#menu");
-    const links = menu.querySelectorAll("#menu div li");
+    const fsmenu = document.querySelector(".fsmenu");
 
-    var tl = gsap.timeline({ paused: true });
-
-    tl.to(menu, {
-      duration: 1,
-      opacity: 1,
-      scaleX: "1", // change this to 100vh for full-height menu
-      ease: "expo.inOut",
-    });
-    tl.from(
-      links,
-      {
-        duration: 1,
-        opacity: 0,
-        y: 20,
-        stagger: 0.1,
-        ease: "expo.inOut",
-      },
-      "-=0.5"
-    );
-
-    tl.reverse();
-
-    ham.addEventListener("click", () => {
-      tl.reversed(!tl.reversed());
+    ham.addEventListener("click", function () {
+      if (this.classList.contains("is-active")) {
+        fsmenu.classList.remove("is-active");
+        fsmenu.classList.add("close-menu");
+      } else {
+        fsmenu.classList.remove("close-menu");
+        fsmenu.classList.add("is-active");
+      }
+      this.classList.toggle("is-active");
     });
   }, []);
   return (
-    <ul className={styles.mobileNavElements} id="menu">
-      <div className={styles.elements_container}>
-        {navigation_elements.map((element) => (
-          <li className={styles.navItem}>
-            <NavLink
-              activeClassName={styles.itemLinkActive}
-              className={styles.itemLink}
-              to={element.path}
-              onClick={toggleNav}
-            >
-              {element.name}
-            </NavLink>
-          </li>
-        ))}
+    // <ul className={styles.mobileNavElements} id="menu">
+    //   <div className={styles.elements_container}>
+    //     {navigation_elements.map((element) => (
+    //       <li className={styles.navItem}>
+    //         <NavLink
+    //           activeclassName={styles.itemLinkActive}
+    //           className={styles.itemLink}
+    //           to={element.path}
+    //           onClick={toggleNav}
+    //         >
+    //           {element.name}
+    //         </NavLink>
+    //       </li>
+    //     ))}
+    //   </div>
+    // </ul>
+    <div class="fsmenu">
+      <div class="fsmenu--container">
+        <div class="fsmenu--text-block">
+          <div class="fsmenu--text-container">
+            <ul class="fsmenu--list">
+              {navigation_elements.map((element) => (
+                <li class="fsmenu--list-element">
+                  <NavLink activeStyle={{ color: "#bfa67a" }} to={element.path}>
+                    <span>{element.name}</span>
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-    </ul>
+    </div>
   );
 };
 
