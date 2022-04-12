08cef834ef873604e1a7a3c81828218a2945f70c
Sebskora <sebastian.skora27@gmail.com>
2022-04-12 17:25:24 +0200

    nav fix

--- src/components/layout/navigation/Navigation.js
+++ src/components/layout/navigation/Navigation.js
@@ -1,67 +1,74 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link as NavLink } from "gatsby";
 import * as styles from "./Navigation.module.scss";
 import * as actions from "../../redux/actions/index";
 import { connect } from "react-redux";
+
 import DropDownNav from "../../smallComponents/DropDownNav/DropDownNav";
 
-const isBrowser = () => typeof window !== "undefined";
-
-const checkPathForDropdown = (elements) => {
-  const url = isBrowser && `/${window.location.href.split("/").at(-2)}/`;
-  let result = false;
-  elements.filter((el) => {
-    if (el === url) {
-      result = true;
-    }
-  });
-  return result;
-};
-
-const Navigation = () => (
-  <ul className={styles.navList}>
-    <li className={styles.navItem}>
-      <NavLink
-        activeClassName={styles.itemLinkActive}
-        className={styles.itemLink}
-        to="/"
-      >
-        Strona główna
-      </NavLink>
-    </li>
-    <li className={styles.navItem}>
-      <NavLink
-        activeClassName={styles.itemLinkActive}
-        className={styles.itemLink}
-        to="/uslugi/"
-      >
-        Usługi
-      </NavLink>
-    </li>
-    <DropDownNav pages={["słownik", "blog"]}>
-      <li className={styles.navItem}>
-        <div
-          activeClassName={styles.itemLinkActive}
-          className={
-            checkPathForDropdown(["/slownik/", "/blog/"])
-              ? `${styles.itemLink} ${styles.itemLinkActive}`
-              : `${styles.itemLink}`
+const Navigation = () => {
+  const checkPathForDropdownWithWindow = (elementsTaken) => {
+    if (typeof window !== "undefined") {
+      const checkPathForDropdown = (elements) => {
+        let result = false;
+        elements.filter((el) => {
+          if (window.location.href.includes(el)) {
+            result = true;
           }
+        });
+        return result;
+      };
+
+      return checkPathForDropdown(elementsTaken);
+    }
+  };
+
+  return (
+    <ul className={styles.navList}>
+      <li className={styles.navItem}>
+        <NavLink
+          activeClassName={styles.itemLinkActive}
+          className={styles.itemLink}
+          to="/"
         >
-          Artykuły
-        </div>
+          Strona główna
+        </NavLink>
       </li>
-    </DropDownNav>
-    <li className={styles.navItem}>
-      <NavLink
-        className={`${styles.itemLink} ${styles.contact}`}
-        to="/kontakt/"
-      >
-        Kontakt
-      </NavLink>
-    </li>
-  </ul>
-);
+      <li className={styles.navItem}>
+        <NavLink
+          activeClassName={styles.itemLinkActive}
+          className={styles.itemLink}
+          to="/uslugi/"
+        >
+          Usługi
+        </NavLink>
+      </li>
+
+      <DropDownNav pages={["słownik", "blog"]}>
+        <li className={styles.navItem}>
+          <div
+            activeClassName={styles.itemLinkActive}
+            className={
+              checkPathForDropdownWithWindow(["/slownik/", "/blog/"])
+                ? `${styles.itemLink} ${styles.itemLinkActive}`
+                : `${styles.itemLink}`
+            }
+          >
+            Artykuły
+          </div>
+        </li>
+      </DropDownNav>
+      <li className={styles.navItem}>
+        <NavLink
+          className={`${styles.itemLink} ${styles.contact}`}
+          to="/kontakt/"
+        >
+          Kontakt
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
